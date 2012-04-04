29f68295de6afc98997f551acae48b56d3e82147
Ben Klang <bklang@mojolingo.com>
2012-04-04 17:01:01 -0400

    Make script more idempotent

--- docs/source/getting-started/installation.sh
+++ docs/source/getting-started/installation.sh
@@ -1,10 +1,19 @@
+source ~/.rvm/scripts/rvm
+rvm remove jruby
+rvm remove 1.9.3
+
 ### @export "rvm-install-jruby"
-#rvm install jruby
-#rvm use jruby
+rvm install jruby
+rvm use jruby
+# JRuby must run in Ruby 1.9 mode:
+export JRUBY_OPTS="--1.9"
 
 ### @export "rvm-install-193"
-#rvm install 1.9.3
-#rvm use 1.9.3
+rvm install 1.9.3
+rvm use 1.9.3
+
+### @export "ignore"
+bundle install
 
 ### @export "install-gem"
 gem install --prerelease adhearsion
