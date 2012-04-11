3f9c899e0e89b013cd2c36cfc93b2df0ebc88a5b
Ben Langfeld <ben@langfeld.me>
2012-04-11 15:05:04 +0100

    Uncomment something we wanted to keep

--- docs/source/getting-started/installation.sh
+++ docs/source/getting-started/installation.sh
@@ -1,15 +1,15 @@
 source ~/.rvm/scripts/rvm
-# rvm remove jruby
-# rvm remove 1.9.3
+rvm remove jruby
+rvm remove 1.9.3
 
 ### @export "rvm-install-jruby"
-# rvm install jruby
+rvm install jruby
 rvm use jruby
 # JRuby must run in Ruby 1.9 mode:
 export JRUBY_OPTS="--1.9"
 
 ### @export "rvm-install-193"
-# rvm install 1.9.3
+rvm install 1.9.3
 rvm use 1.9.3
 
 ### @export "install-gem"
